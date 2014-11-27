d8780b50b939d9dae4de00bba7440309bea9afbc
Daniel Charlton <charlton.dan@reiversolutions.co.uk>
2014-11-27 13:20:52 +0000

    Example solution for using the passle feed with a basic mvc 4 applicaiton

---
+++ Example/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        // 
+        // GET: /
+
+        public ActionResult Index()
+        {
+            // Setup basic route to redirect to show feed
+            return RedirectToAction("ShowFeed");
+        }
+
+        //
+        // GET: /Home/ShowFeed
+
+        public ActionResult ShowFeed()
+        {
+            return View();
+        }
+    }
+}
---
+++ Example/Extensions/HtmlHelperXmlExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace Example.Extensions
+{
+    public static class HtmlHelperXmlExtensions
+    {
+        /// <summary>
+        /// Render Passle feed
+        /// </summary>
+        /// <param name="helper">Html helper class</param>
+        /// <param name="xmlPath">Path to xml</param>
+        /// <param name="xsltPath">Path to xslt</param>
+        /// <param name="parameters">Optional paramaters</param>
+        /// <returns>Html string output</returns>
+        public static HtmlString RenderXml(this HtmlHelper helper, string xmlPath, string xsltPath, IDictionary<string, string> parameters)
+        {
+            XsltArgumentList args = new XsltArgumentList();
+            if (parameters != null)
+            {
+                foreach (string key in parameters.Keys)
+                {
+                    args.AddParam(key, string.Empty, parameters[key]);
+                }
+            }
+
+            XslCompiledTransform t = new XslCompiledTransform();
+            t.Load(xsltPath);
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            settings.ValidationType = ValidationType.DTD;
+
+            using (XmlReader reader = XmlReader.Create(xmlPath, settings))
+            {
+                StringWriter writer = new StringWriter();
+                t.Transform(reader, args, writer);
+                return new HtmlString(writer.ToString());
+            }
+        }
+    }
+}
